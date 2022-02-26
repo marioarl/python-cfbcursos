087baa186bb75c7cc839d52a47e41f82fe0cbeeb
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-26 11:42:10 -0300

    Criacao aula02.py

---
+++ Aulas - Parte 1/aula 02 - Sintaxe basica e comentarios/aula02.py
@@ -0,0 +1,21 @@
+#Este é um comentario de linha - A terminacao de um comando em outras linguagens é o ponto-e-virgula(;) no Python utiliza-se o ENTER, mas tambem podemos utilizar o ; se quisermos colocar mais de 1 comando em uma linha (ex canal="CFB Cursos;curso="Curso de Python").
+
+"""
+Este é um
+comentário 
+de 
+multiplas
+linhas
+
+"""
+
+canal="CFB Cursos"
+curso="Curso de Python"
+print(canal + " - " + curso)
+
+num=num2=res=0 #Inicializar varias variaveis com o mesmo valor
+
+if 10 > 2:
+    print("Maior") #Sempre adicionar um recuo dentro de um escopo de comandos, voce pode delimitar a quantidade de espaços(por padrao sao 4 espaços ou a tecla TAB)
+    print("Aula 2")
+print("FIM")
