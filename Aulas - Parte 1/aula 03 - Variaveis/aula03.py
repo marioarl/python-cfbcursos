183ada3bd5034f8f3c0bf527c8691c96089d73a7
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-02-26 11:50:55 -0300

    Criacao aula03.py

---
+++ Aulas - Parte 1/aula 03 - Variaveis/aula03.py
@@ -0,0 +1,19 @@
+#Qualquer variavel que criamos na parte de cima do programa, sao variaveis GLOBAIS, podem ser acessadas de qualquer parte do programa 
+num1=num2=res=0
+
+
+#criando uma função
+def cn():
+    #criando uma variavel dentro da função
+    global canal #Quando declaramos uma variavel como GLOBAL, nao podemos inicializa-la na mesma linha
+    canal = "CFB Cursos"
+    print(canal)
+
+
+#Chamando a função cn()
+cn()
+
+#Se tentarmos utilizar a variavel canal fora da função, o Python irá dar ERRO. Mas conseguimos acessar essa variavel se esta mesma variavel for definida dentro da funcao como GLOBAL
+print(canal)
+
+
