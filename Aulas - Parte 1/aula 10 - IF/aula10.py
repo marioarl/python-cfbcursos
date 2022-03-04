0a0c2972b8f19022f3c88d01586014538dbb28e3
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-04 11:08:21 -0300

    Criacao aula10.py

---
+++ Aulas - Parte 1/aula 10 - IF/aula10.py
@@ -0,0 +1,47 @@
+a = False
+
+if a :
+    print("CFB Cursos")
+
+print("FIM do programa")
+print("---------------------")
+
+b = 10
+c = 5
+op = "+"
+
+if b > c:
+    print("Maior")
+else:
+    print("Menor")
+print("FIM do programa")
+
+print("---------------------")
+
+if op == "+":
+    res=b+c
+print(f"{b} + {c} = {res}")
+print("FIM do programa")
+print("---------------------")
+
+if op == "-":
+    res=b-c
+print(f"{b} - {c} = {res}")
+print("FIM do programa")
+print("---------------------")
+
+if op == "*":
+    res=b*c
+print(f"{b} X {c} = {res}")
+print("FIM do programa")
+print("---------------------")
+
+if op == "/":
+    res=b/c
+print(f"{b} / {c} = {res}")
+print("FIM do programa")
+print("---------------------")
+
+
+
+
