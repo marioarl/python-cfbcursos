92214d346898b9c6979456156a95a23e0ab19f7e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-15 11:17:32 -0300

    Criacao aula11.py

---
+++ Aulas - Parte 1/aula 11 - IF ELIF ELSE/aula11.py
@@ -0,0 +1,24 @@
+
+b = 10
+c = 5
+op = "%"
+
+if op == "+":
+    res=b+c
+
+elif op == "-":
+    res=b-c
+
+elif op == "*":
+    res=b*c
+
+elif op == "/":
+    res=b/c
+
+else:
+    print("Operador invalido")
+print(f"{b} {op} {c} = {res}")
+
+
+
+
