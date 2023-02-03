42cf80d839f10726d44339f97fad511e7754807b
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-02-03 13:57:10 -0300

    Criacao aula13.py

---
+++ Aulas - Parte 1/aula 13 - Função INPUT/aula13.py
@@ -0,0 +1,9 @@
+'''
+A função input() é a entrada do usuario, quando o usuario deverá passar algum
+tipo de dado ao progrma.
+'''
+
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+res = num1 + num2
+print(f'A soma dos valores é {res}')
