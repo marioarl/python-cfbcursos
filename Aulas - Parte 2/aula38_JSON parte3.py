4e4e5aa447266e7d14e46996643dfe0e902f02b0
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-02 10:14:48 -0300

    Update aula38_JSON parte3.py

--- Aulas - Parte 2/aula38_JSON parte3.py
+++ Aulas - Parte 2/aula38_JSON parte3.py
@@ -5,7 +5,7 @@ Trabalhando com JSON PARTE 3
 
 import json
 
-jogador_j = '{"nome": "Bruno","time": "aviadores","vivo": "True","energia": 100,"mochila": ["pederneira","corda", "linha", "arame"],"aeronaves": [{"tipo": "transporte", "habilidade": 80},{"tipo": "ataque","habilidade": 100},{"tipo": "reconhecimento", "habilidade": 100}]}'
+jogador_j = '{"nome": "Bruno","time": "aviadores","vivo": "True","energia": 100,"mochila": ["faca","corda", "linha", "arame"],"aeronaves": [{"tipo": "transporte", "habilidade": 80},{"tipo": "ataque","habilidade": 100},{"tipo": "reconhecimento", "habilidade": 100}]}'
 
 jogador = json.loads(jogador_j)
 
