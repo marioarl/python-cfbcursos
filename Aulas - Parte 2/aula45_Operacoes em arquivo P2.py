3522f5544b538ad9a79235a41bea6fa79a6ceb80
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2023-03-11 11:25:30 -0300

    Update aula45_Operacoes em arquivo P2.py

--- Aulas - Parte 2/aula45_Operacoes em arquivo P2.py
+++ Aulas - Parte 2/aula45_Operacoes em arquivo P2.py
@@ -30,7 +30,7 @@ res2 = re.sub(' ', '-', h.readline())
 h.close()
 print(res2)
 
-#Escrever o retorno da variavel res2 dentro do arquivo teste3.txt, mas sem apagar os dados do aqruivo
+#Escrever o retorno da variável res2 dentro do arquivo teste3.txt, mas sem apagar os dados do aqruivo
 
 h = open('Aqui deve conter o caminho do arquivo' + nome3, 'at')
 h.write(f'{res2}')
